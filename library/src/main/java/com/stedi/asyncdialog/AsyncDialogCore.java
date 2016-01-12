07b67dd24843f93814df0e3545d350e317e0535f
Dima Stepanchenko <stepanchenkodmitriy@gmail.com>
2016-01-12 16:08:36 +0100

    little edit

--- library/src/main/java/com/stedi/asyncdialog/AsyncDialogCore.java
+++ library/src/main/java/com/stedi/asyncdialog/AsyncDialogCore.java
@@ -11,7 +11,7 @@ import android.os.Handler;
 import android.util.Log;
 
 /**
- * Dialog that manages operations in the background,
+ * Dialog that handles operations in background,
  * and then returns the result of this operation to a fragment or activity.
  *
  * @param <Result> that the background operation must return.
